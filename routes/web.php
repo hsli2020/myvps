e9ce4e047fcaf232395ce736322fdcf844e797f3
Jiebang Li <jiebang@gmail.com>
2026-03-10 13:04:11 -0400

    breeze auth

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,26 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\FileController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 Route::middleware(['auth'])->group(function () {
     Route::resource('files', FileController::class);
     Route::get('/download/{id}', [FileController::class, 'download'])->name('files.download');
 });
+
+require __DIR__.'/auth.php';
